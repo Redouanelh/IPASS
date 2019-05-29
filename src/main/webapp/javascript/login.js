6bd56c318d6331b36ee990c13563cf30ca4e2450
Redouanelh <redouan_school@outlook.com>
2019-05-29 23:25:31 +0200

    Login HTML/CSS finish

--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -1,1 +1,3 @@
 // Annotaties maken voor iedere functie!
+// De label weghalen van onjuist gebruikersnaam/wachtwoord en die alleen pas tonen als je na response geen response.ok krijgt, zie practicum8login.js!
+// Hij staat nu op display none, maar dit moet je dan dus display block maken!
