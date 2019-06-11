29c20cced13073a209e89e023c65501136a88364
Redouanelh <redouan_school@outlook.com>
2019-06-11 23:32:24 +0200

    Maps gefixt

--- src/main/webapp/javascript/spelerprofiel.js
+++ src/main/webapp/javascript/spelerprofiel.js
@@ -18,6 +18,7 @@ function loadProfile() {
     .then(function(myJson) {
       // Hier de functie die de profielgegevens op de webpagina plaatst.
       setProfileVariables(myJson);
+      setMapStreet(myJson.straat);
     });
   }
 
@@ -48,6 +49,11 @@ function setProfileVariables(myJson) {
   setValue("#straathuisnummer", myJson.huisnummer);
   setValue("#postcode", myJson.postcode);
   setValue("#woonplaats", myJson.woonplaats);
+}
+
+// Voor de maps zodat je de juiste straat mee krijgt.
+function setMapStreet(street) {
+  document.querySelector("#gmap_canvas").setAttribute("src", "https://maps.google.com/maps?q=" + street + "&t=&z=13&ie=UTF8&iwloc=&output=embed");
 }
 
 // Een functie die bepaalde values in de webpagina plaatst met behulp van een id.
